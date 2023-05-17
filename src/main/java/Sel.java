99b7f55fdb4c641566588ccf25ea766612228691
Thrawn7 <95602482+Thrawn7@users.noreply.github.com>
2023-05-17 13:25:50 -0500

    works

--- src/main/java/Sel.java
+++ src/main/java/Sel.java
@@ -2,18 +2,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.checkerframework.checker.units.qual.g;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByClassName;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class Sel {
-    
+    private static WebDriver driver = new ChromeDriver();
+    private static String xPath = "/html/body/main/div/div/div[2]/div/div[1]/div/div[2]/div[2]/form[1]";
     public static void main(String[] args) throws InterruptedException {
-        
         WebDriverManager.chromiumdriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver.get("https://projectstem.org/users/sign_in");
 
-        driver.get("https://saucelabs.com/test/guinea-pig");
+        /*       WebElement user_login = driver.findElement(By.id("user_login"));
+        WebElement user_password = driver.findElement(By.id("user_password"));
+        user_login.clear(); user_password.clear();
+        user_login.sendKeys("user");
+        user_password.sendKeys("passward"); */
         
-        Thread.sleep(5000);
+        WebElement google = driver.findElement(By.xpath(xPath));
+        System.out.println(google.getText());  
+        
+        System.out.println();
 
+        google.click(); 
+        //Thread.sleep(5000);
+        
+        //driver.get("https://courses.projectstem.org/courses/63543/grades");
+        Thread.sleep(5000);
         driver.quit();
+        
+        //Assert.assertEquals(1, 1);
     }
 
 }
--- src/test/java/SelTest.java
+++ src/test/java/SelTest.java
@@ -1,20 +1,24 @@
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SelTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class SelTest {
     @Test
     public static void main(String[] args) throws InterruptedException {
-
+        
         WebDriverManager.chromiumdriver().setup();
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://saucelabs.com/test/guinea-pig");
-        driver.quit();
-        //assertEquals(1, 1);
+        WebElement forms = driver.findElement(By.id("the_forms_id"));
+        forms.click();
+        driver.quit(); 
+        //Assert.assertEquals(1,1);
     }
+
 }
